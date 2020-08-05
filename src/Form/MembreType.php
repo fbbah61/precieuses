b6a9bff407f7ec04416e20b9f4862807f9d2c803
Fbbah61 <Fbbah61@gmail.com>
2020-08-06 00:16:08 +0200

    inscription et connexion d'un membre

---
+++ src/Form/MembreType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Membre;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MembreType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('civilite')
+            ->add('nom')
+            ->add('prenom')
+            ->add('email')
+            ->add('adresse')
+            ->add('code_postal')
+            ->add('password',PasswordType::class)
+            ->add('confirm_password',PasswordType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Membre::class,
+        ]);
+    }
+}
